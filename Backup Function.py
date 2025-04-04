6b1de331cb9c2e9a2bd94fab2b9d6d37e7bcf316
Nastaran Mohammadiafshar <nmohammadiafshar@myseneca.ca>
2025-04-04 18:28:32 -0400

    Nastaran Afshar created a function called copy_files that will copy all the files in the source that was provided and will save them in the destination that is provided as well

--- Backup Function.py
+++ Backup Function.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 import os
 
-
-
+def copy_files(source, destination):
+    """Recursively copies files from source to destination."""
+    for item in os.listdir(source):
+        source_path = os.path.join(source, item)
+        destination_path = os.path.join(destination, item)
