4406d430e98c9dfc65ff5daa45f7505beba7cb56
Nastaran Mohammadiafshar <nmohammadiafshar@myseneca.ca>
2025-04-04 20:48:06 -0400

    Nastaran Afshar added if/esle clause that checks if the destination exists and will create it if it doesnt. Then will save the content in source to the destination.

--- Backup Function.py
+++ Backup Function.py
@@ -6,3 +6,16 @@ def copy_files(source, destination):
     for item in os.listdir(source):
         source_path = os.path.join(source, item)
         destination_path = os.path.join(destination, item)
+
+        if os.path.isdir(source_path):
+            if os.path.exists(destination_path) == False:
+                os.makedirs(destination_path)
+            copy_files(source_path, destination_path)
+        else:
+            src_file = open(source_path, 'r')
+            content = src_file.read()
+            src_file.close()
+
+            dest_file = open(destination_path, 'w')
+            dest_file.write(content)
+            dest_file.close()
