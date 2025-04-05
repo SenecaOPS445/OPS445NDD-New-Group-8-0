91f5310d8fe9272a747382d56a859d805feb1798
Nastaran Mohammadiafshar <nmohammadiafshar@myseneca.ca>
2025-04-05 08:38:34 -0400

    Nastaran Afshar ceated a backuo function that will save each file in the directory by naming them backup_n

--- Backup Function.py
+++ Backup Function.py
@@ -19,3 +19,11 @@ def copy_files(source, destination):
             dest_file = open(destination_path, 'w')
             dest_file.write(content)
             dest_file.close()
+
+    def backup(source, destination):
+        """Creates a backup of the source directory in the destination."""
+        count = len(os.listdir(destination)) + 1
+        backup_name = "backup_" + str(count)
+        backup_path = os.path.join(destination, backup_name)
+        os.makedirs(backup_path)
+        print("Backup successful: " + backup_path)
