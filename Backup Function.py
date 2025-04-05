1f30bce108f6803193e9b4e8a9ca81daf35aad3f
Nastaran Mohammadiafshar <nmohammadiafshar@myseneca.ca>
2025-04-05 10:05:03 -0400

    Nastaran Afshar editting the script so it would handle errors for when th backup file name exists more accuretly

--- Backup Function.py
+++ Backup Function.py
@@ -22,37 +22,49 @@ def copy_files(source, destination):
             dest_file.close()
 
 def backup(source, destination):
-        """Creates a backup of the source directory in the destination."""
-        source_exists = os.path.exists(source)
-        if source_exists == False:
-            print("Error: Source directory '" + source + "' does not exist.")
-            return
+    """Creates a backup of the source directory in the destination."""
+    source_exists = os.path.exists(source)
+    if source_exists == False:
+        print("Error: Source directory '" + source + "' does not exist.")
+        return
 
-        destination_exists = os.path.exists(destination)
-        if destination_exists == False:
-            print("Warning: Destination directory '" + destination + "' does not exist and will be created.")
+    destination_exists = os.path.exists(destination)
+    if destination_exists == False:
+        print("Warning: Destination directory '" + destination + "' does not exist and will be created.")
         try:
             os.makedirs(destination)
         except PermissionError:
             print("Error: You do not have permission to create the destination directory.")
             return
-                    
-        count = len(os.listdir(destination)) + 1
-        backup_name = "backup_" + str(count)
-        backup_path = os.path.join(destination, backup_name)
-        os.makedirs(backup_path)
-        try:
-            os.makedirs(backup_path)
-        except PermissionError:
-            print("Error: You do not have permission to create the backup folder in the destination.")
-            return
 
-        try:
-            copy_files(source, backup_path)
-        except:
-            print("Error occurred during file copy.")
-            return
-        print("Backup successful: " + backup_path)
+    source_name = os.path.basename(os.path.normpath(source))
+    backup_name = source_name + "_backup"
+    backup_path = os.path.join(destination, backup_name)
+
+    count = 1
+    original_backup_path = backup_path
+    while os.path.exists(backup_path):
+        backup_path = original_backup_path + "_" + str(count)
+        count += 1
+
+    try:
+        path_exists = os.path.exists(backup_path)
+        if path_exists == False:
+            os.makedirs(backup_path)
+    except PermissionError:
+        print("Error: You do not have permission to create the backup folder in the destination.")
+        return
+    except FileExistsError:
+        print("Error: Backup folder already exists and cannot be created.")
+        return
+    
+    try:
+        copy_files(source, backup_path)
+    except:
+        print("Error occurred during file copy.")
+        return
+
+    print("Backup successful: " + backup_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simple backup script")
