bfbb553fddde62a40d2f1655a9543937eb248389
Deepak Sangraula <dsangraula@myseneca.ca>
2025-04-07 00:14:02 -0400

    Deepak Sangraula added Initialize restore_zip script with basic structure and imports

--- Restore Function.py
+++ Restore Function.py
@@ -1,1 +1,13 @@
-#!/usr/bin/env python3
+import os
+import zipfile
+import argparse
+
+def find_zip_in_directory(directory):
+    pass
+
+def restore_zip(source, destination):
+    pass
+
+if __name__ == "__main__":
+    pass
+
