eff28aa12ae8bc311ef7cb913a3811829c6a0976
Deepak Sangraula <dsangraula@myseneca.ca>
2025-04-07 11:08:48 -0400

    Deepak Sangraula added Implement find_zip_in_directory function to locate zip file

--- Restore Function.py
+++ Restore Function.py
@@ -1,2 +1,6 @@
-import os
-import zipfile
+def find_zip_in_directory(directory):
+    """Searches for the first .zip file in a directory."""
+    for item in os.listdir(directory):
+        if item.endswith(".zip"):
+            return os.path.join(directory, item)
+    return None
