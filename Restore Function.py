840d89311756044fcf8e039b46211619f859b36e
Deepak Sangraula <dsangraula@myseneca.ca>
2025-04-07 01:03:35 -0400

    Deepak Sangraula added Remove CLI main block to make script importable as a module

--- Restore Function.py
+++ Restore Function.py
@@ -1,5 +1,1 @@
-def find_zip_in_directory(directory):
-    """Searches for the first .zip file in a directory."""
 
-def restore_zip(source, destination):
-    """Restores (unzips) a .zip file from the source to the destination."""
