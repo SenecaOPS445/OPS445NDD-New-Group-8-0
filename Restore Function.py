72dac6f6054dbcc104dd2f2525ace925e99ef885
Deepak Sangraula <dsangraula@myseneca.ca>
2025-04-07 11:17:00 -0400

    Deepak Sangraula added exception handling for various errors during extraction

--- Restore Function.py
+++ Restore Function.py
@@ -1,7 +1,8 @@
-    if os.path.exists(destination) == False:
-        os.makedirs(destination)
-
-    try:
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            zip_ref.extractall(destination)
-            print(f"[SUCCESS] Restored contents of '{zip_path}' to '{destination}'")
+    except zipfile.BadZipFile:
+        print(f"[ERROR] Failed to unzip: Bad zip file '{zip_path}'")
+    except PermissionError:
+        print(f"[ERROR] Permission denied while extracting to '{destination}'")
+    except FileNotFoundError:
+        print(f"[ERROR] File not found: '{zip_path}'")
+    except Exception:
+        print("[ERROR] An unexpected error occurred.")
