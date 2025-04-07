d86b1272b489e47e4fb7771e7d8289d7ed4d5d6e
Deepak Sangraula <dsangraula@myseneca.ca>
2025-04-07 00:25:17 -0400

    Deepak Sangraula added restore_zip()

--- Restore Function.py
+++ Restore Function.py
@@ -1,6 +1,14 @@
-def find_zip_in_directory(directory):
-    """Searches for the first .zip file in a directory."""
-    for item in os.listdir(directory):
-        if item.endswith(".zip"):
-            return os.path.join(directory, item)
-    return None
+if not os.path.exists(source):
+    print(f"[ERROR] Source path '{source}' does not exist.")
+    return
+
+if zipfile.is_zipfile(source):
+    zip_path = source
+elif os.path.isdir(source):
+    zip_path = find_zip_in_directory(source)
+    if zip_path is None:
+        print("[ERROR] No zip file found in the directory.")
+        return
+else:
+    print("[ERROR] Source must be a .zip file or a directory containing one.")
+    return
